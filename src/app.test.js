3883fe3cb85716e3e453a7e075debecc2843b633
Attila Eross <attila.eross@yahoo.co.uk>
2021-02-03 12:10:51 +0000

    test should delete an item from todo List

--- src/app.test.js
+++ src/app.test.js
@@ -28,3 +28,25 @@ test("shows new item in todo list when added", () => {
   const todoTextElement = getByText(todoList, "Take wife for a walk!");
   expect(todoTextElement).toHaveTextContent(inputText);
 });
+
+test("should delete an item from todo List", () => {
+  // setup
+  const input = screen.getByPlaceholderText("Please enter todo here");
+  const addButton = screen.getByTestId("add-button");
+  const inputText = "Todo to be deleted!";
+
+  // act
+  userEvent.type(input, inputText);
+  userEvent.click(addButton);
+  const todoList = screen.getByTestId("todo-list");
+  const todoTextElement = getByText(todoList, "Todo to be deleted!");
+  expect(todoList.childElementCount).toBe(2);
+
+  const todoElement = todoTextElement.parentElement;
+  const todoDeleteButton = getByText(todoElement, "Delete");
+  userEvent.click(todoDeleteButton);
+
+  // assert
+  expect(todoList).not.toContainElement(todoElement);
+  expect(todoList.childElementCount).toBe(1);
+});
