241b0ea502a898381961c8ad03e425ce1001e603
Attila Eross <attila.eross@yahoo.co.uk>
2021-02-07 15:00:08 +0000

    refactored test - simplified test case by using/deleting todo added in the previous test case

--- src/app.test.js
+++ src/app.test.js
@@ -29,22 +29,14 @@ test("shows new item in todo list when added", () => {
 
 test("removes an item from todo List when user clicks Delete button", () => {
   // setup
-  const input = screen.getByPlaceholderText("Please enter todo here");
-  const addButton = screen.getByText("Add");
-  const inputText = "Todo to be deleted!";
 
   // act
-  userEvent.type(input, inputText);
-  userEvent.click(addButton);
   const todoList = screen.getByTestId("todo-list");
-  const todoTextElement = getByText(todoList, "Todo to be deleted!");
-  expect(todoList.childElementCount).toBe(2);
-
-  const todoElement = todoTextElement.parentElement;
-  const todoDeleteButton = getByText(todoElement, "Delete");
-  userEvent.click(todoDeleteButton);
+  const deleteButton = getByText(todoList, "Delete");
+  userEvent.click(deleteButton);
 
   // assert
-  expect(todoList).not.toContainElement(todoElement);
-  expect(todoList.childElementCount).toBe(1);
+  expect(
+    queryByText(todoList, "Take wife for a walk!")
+  ).not.toBeInTheDocument();
 });
